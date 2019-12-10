f2e68a46780f2cd78c4c18e251b6c093b79d35f0
martademadariaga <43268987+martademadariaga@users.noreply.github.com>
2019-12-10 10:10:07 +0100

    Add files via upload

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from . import views
 
 
 urlpatterns = [
    path('',views.index, name ='index'),
    path('busqueda/', views.Insertar.as_view(), name='insertar'),
+   path('adivinar/', views.Adivinar.as_view(), name='adivinar'),
+   path('graficos/', views.Pintar, name='pintar'),
   ]
 
 
